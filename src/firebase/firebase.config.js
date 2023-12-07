94c8d712382776c6430d2d9e38eadd04e1484cdc
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-12-07 15:55:03 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD8LDoJNrZ5I70Il2vEQeLbrN3CF_TbCl0",
+    authDomain: "auth-firebase-context-ta-99183.firebaseapp.com",
+    projectId: "auth-firebase-context-ta-99183",
+    storageBucket: "auth-firebase-context-ta-99183.appspot.com",
+    messagingSenderId: "365997833762",
+    appId: "1:365997833762:web:27f47465ee5bf38380947b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+// import DaisyUI from "daisyui";
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // plugins: [DaisyUI],
+}
+
